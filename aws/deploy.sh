f34283cb9629473750e0d422afa29fb82dcccf22
Marques, Charles <cmarques@lmi.org>
2024-09-04 20:53:09 -0400

    deploy.sh

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -328,7 +328,7 @@ fi
 
 ## Configure OCP cluster
 log "==== OCP cluster configuration (Cert Manager) started ===="
-cd $GIT_REPO_HOME/../ibm/mas_devops/playbooks
+cd /root/ansible-playbook/ibm/mas_devops/playbooks
 set +e
 
 if [[ $ROSA == "true" ]]; then
